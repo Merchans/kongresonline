a6e59d0040834b328000a17f8aaa1a209187840e
Merchnas <addmarkovic@gmail.com>
2020-09-02 09:52:47 +0200

    vytvorenie template pre single.php a single-video.php

--- functions.php
+++ functions.php
@@ -425,3 +425,42 @@ add_filter( 'body_class', function( $classes ) {
     return $classes;
 } );
 
+
+
+// Custom single template by category
+// https://halgatewood.com/wordpress-custom-single-templates-by-category
+// For single posts
+add_filter('single_template', 'chi_check_for_category_single_template');
+function chi_check_for_category_single_template( $t )
+{
+    foreach( (array) get_the_category() as $cat )
+    {
+        if ( file_exists(STYLESHEETPATH . "/single-category-{$cat->slug}.php") ) return STYLESHEETPATH . "/single-category-{$cat->slug}.php";
+        if($cat->parent)
+        {
+            $cat = get_the_category_by_ID( $cat->parent );
+            if ( file_exists(STYLESHEETPATH . "/single-category-{$cat->slug}.php") ) return STYLESHEETPATH . "/single-category-{$cat->slug}.php";
+        }
+    }
+    return $t;
+}
+
+
+// Custom single template by category
+// https://halgatewood.com/wordpress-custom-single-templates-by-category
+// For video posts
+add_filter('single_template', 'chi_check_for_category_single_video_template');
+function chi_check_for_category_single_video_template( $t )
+{
+
+    foreach( (array) get_the_category() as $cat )
+    {
+        if ( file_exists(STYLESHEETPATH . "/single-chi_video-category-{$cat->slug}.php") ) return STYLESHEETPATH . "/single-chi_video-category-{$cat->slug}.php";
+        if($cat->parent)
+        {
+            $cat = get_the_category_by_ID( $cat->parent );
+            if ( file_exists(STYLESHEETPATH . "/single-chi_video-category-{$cat->slug}.php") ) return STYLESHEETPATH . "/single-chi_video-category-{$cat->slug}.php";
+        }
+    }
+    return $t;
+}
