b33fdbcfc37a74ef0a5ba0c85c8069a8734f6e17
Merchans <addmarkovic@gmail.com>
2022-06-03 13:13:46 +0200

    fix: search input

--- js/lou-multi-select/multiple-select-otions.js
+++ js/lou-multi-select/multiple-select-otions.js
@@ -1,5 +1,5 @@
 // run pre selected options;
-	$('#chi_selected_articles_or_videos_page_options, #directors_id, #actors_id, #chi_selected_articles_or_videos, #chi_selected_thems_in_category, #chi_selected_in_category_advertising_horizontal, #chi_selected_in_category_advertising_vertical').multiSelect({
+	jQuery('#chi_selected_articles_or_videos_page_options, #directors_id, #actors_id, #chi_selected_articles_or_videos, #chi_selected_thems_in_category, #chi_selected_in_category_advertising_horizontal, #chi_selected_in_category_advertising_vertical').multiSelect({
 
 	selectableHeader: "<input type='text' class='search-input' autocomplete='off' placeholder=' Vyhledávání...'>",
 	selectionHeader: "<input type='text' class='search-input' autocomplete='off' placeholder=' Vyhledávání...'>",
