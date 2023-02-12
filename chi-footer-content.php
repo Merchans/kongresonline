b181a364dad8521490c018beeb653bfb817facc8
Richard Markovič <richard.markovic@diamondigital.cz>
2023-02-12 21:27:43 +0100

    add cookies setting btn

--- chi-footer-content.php
+++ chi-footer-content.php
@@ -1,7 +1,7 @@
 <div class="container chi-hr mt-25 chi-pt-5"></div>
 <div class="chi-footer-webpage-info container p-0">
 	<p>Tyto stránky jsou určeny výhradně pro odbornou veřejnost.</p>
-	<p id="cookiePreferencesButton"><u>Nastavení cookies</u></p>
+	<u id="cookiePreferencesButton">Nastavení cookies</u>
 	<p> &copy; 2015 – <span id="year"></span> CZECH HEALTH INTERACTIVE</p>
 </div>
 </div>
@@ -25,4 +25,32 @@
 </style>
 <script>
 	document.getElementById("year").innerHTML = new Date().getFullYear();
+
+	function setCookie(key, value, expireDays, expireHours, expireMinutes, expireSeconds) {
+		var expireDate = new Date();
+		if (expireDays) {
+			expireDate.setDate(expireDate.getDate() + expireDays);
+		}
+		if (expireHours) {
+			expireDate.setHours(expireDate.getHours() + expireHours);
+		}
+		if (expireMinutes) {
+			expireDate.setMinutes(expireDate.getMinutes() + expireMinutes);
+		}
+		if (expireSeconds) {
+			expireDate.setSeconds(expireDate.getSeconds() + expireSeconds);
+		}
+		document.cookie = key + "=" + escape(value) +
+			";domain=" + window.location.hostname +
+			";path=/" +
+			";expires=" + expireDate.toUTCString();
+	}
+
+	function deleteCookie(name) {
+		setCookie(name, "", null, null, null, 1);
+	}
+	document.getElementById("cookiePreferencesButton").addEventListener("click", function() {
+		deleteCookie("cc_cookie");
+		location.reload();
+	});
 </script>
