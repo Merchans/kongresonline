2f11f54eb35800382acd0a8622ebea4e8d133f75
Richard Markovič <richard.markovic@diamondigital.cz>
2023-02-27 10:39:56 +0100

    search bar added

--- searchform.php
+++ searchform.php
@@ -1,4 +1,4 @@
-<div class="chi-search-wrapper <?php if (is_front_page() || is_page()) echo 'chi-search-wrapper--front-page' ?>">
+<div class="chi-search-wrapper <?php if (is_front_page() || is_page() || is_search()) echo 'chi-search-wrapper--front-page' ?>">
     <form class="form-inline" method="get" id="chi-search-form" autocomplete="off">
         <div class="chi-search">
             <input class="form-control mr-sm-2 w-100" id="chi-search-input" type="text" placeholder="Vyhledat na celém portále&hellip;" aria-label="Search" name="s">
